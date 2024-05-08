42702bdba4ac908703bc90927168d92c0598f797
Franz Vrolijk <franz.vrolijk@tietoevry.com>
2024-05-08 10:45:37 +0200

    WIP

--- src/ReinforcementLearning/Program.cs
+++ src/ReinforcementLearning/Program.cs
@@ -6,13 +6,13 @@ public class Program
 {
     private static async Task Main()
     {
-        var numNetworks = 20;
+        var numNetworks = 300;
         var boardSize = 10;
-        var numIter = 3;
+        var numIter = 15;
         var generation = 1;
 
         var networks = Enumerable.Range(0, numNetworks)
-            .Select(_ => new Network([4, 8, 4, 2], Sigmoid))
+            .Select(_ => new Network([4, 8, 16, 8, 4], Sigmoid))
             .ToList();
 
         var stopwatch = new Stopwatch();
@@ -43,28 +43,49 @@ public class Program
                 }
             }
 
-            var bestScores = networkScoreDict.OrderByDescending(s => s.Value).Take(numNetworks / 3).ToList();
+            var orderedScores = networkScoreDict.OrderByDescending(s => s.Value).ToList();
+            var bestScores = orderedScores.Take((int)(numNetworks * 0.75)).ToList();
             var bestNetworkIndexes = bestScores.Select(s => s.Key).ToList();
             var otherNetworkIndexes = Enumerable.Range(0, numNetworks).Except(bestNetworkIndexes).ToList();
-            var networksToMutate = networks.Where((_, i) => otherNetworkIndexes.Contains(i)).ToList();
 
-            // Pass param to mutate that will change mutation aggresiveness? "score relative to best network (%)"
-            networksToMutate.ForEach(n => n.Mutate());
+            var bestNetworks = networks.Where(((_, index) => !otherNetworkIndexes.Contains(index))).ToList();
+            var networksToSwap = networks.Where((_, index) => otherNetworkIndexes.Contains(index)).ToList();
 
-            if (generation % 10000 == 0)
+            foreach (var network in networksToSwap)
             {
+                networks.Remove(network);
+
+                var goodNetworkCopy = CopyRandomGoodNetwork(bestNetworks);
+
+                goodNetworkCopy.Mutate();
+
+                networks.Add(goodNetworkCopy);
+            }
+
+            if (generation % 100 == 0)
+            {
+                Console.Clear();
                 Console.WriteLine($"""
                                    Generation {generation}
-                                   Best score: {bestScores[0].Value} (network {bestScores[0].Key})
-                                   Average score: {networkScoreDict.Average(n => n.Value)}
-                                   Nth Generation time: {stopwatch.Elapsed.TotalSeconds} seconds
+                                   Best score: {bestScores[0].Value:0.00} (network {bestScores[0].Key})
+                                   Average score: {networkScoreDict.Average(n => n.Value):0.00}
+                                   Nth Generation time: {stopwatch.Elapsed.TotalSeconds:0.00} seconds
                                    """);
-                Console.WriteLine();
+                
                 stopwatch.Restart();
             }
 
             generation++;
         }
+    }
+
+    private static Network CopyRandomGoodNetwork(List<Network> networks)
+    {
+        var randomIndex = Random.Shared.Next(0, networks.Count);
+
+        var networkToCopy = networks[randomIndex];
+
+        return networkToCopy.Copy();
     }
 
     public static Board GetBoard(int size)
@@ -87,16 +108,17 @@ public class Program
     {
         var o1 = output[0];
         var o2 = output[1];
+        var o3 = output[2];
+        var o4 = output[3];
 
-        const double threshold = 0.5d;
+        const double threshold = 0.9d;
 
-        return o1 switch
-        {
-            < threshold when o2 >= threshold => Direction.Up,
-            >= threshold when o2 < threshold => Direction.Down,
-            < threshold when o2 < threshold => Direction.Left,
-            _ => Direction.Right
-        };
+        if (o1 >= threshold) return Direction.Up;
+        if (o2 >= threshold) return Direction.Down;
+        if (o3 >= threshold) return Direction.Left;
+        if (o4 >= threshold) return Direction.Right;
+
+        return Direction.Up;
     }
 
     public static double Sigmoid(double value)
